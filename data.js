59f79fbc97404b05b00d73191dc2add396f182b7
veevr <87350048+veevr@users.noreply.github.com>
2021-07-13 13:49:59 -0700

    V1
    Three Scans

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-veev_fab_start",
-      "name": "veev_fab_start",
+      "id": "0-veev_fab_5",
+      "name": "veev_fab_5",
       "levels": [
         {
           "tileSize": 256,
@@ -26,18 +26,38 @@ var APP_DATA = {
           "size": 4096
         }
       ],
-      "faceSize": 2585.5,
+      "faceSize": 2587,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -2.280388042177492,
+        "pitch": 0.16025240484733771,
+        "fov": 0.9410955196928921
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": 2.5049514703567315,
+          "pitch": 0.5343197079663913,
+          "rotation": 6.283185307179586,
+          "target": "1-veev_fab_1"
+        },
+        {
+          "yaw": -2.7324725786726756,
+          "pitch": 0.49405674472683714,
+          "rotation": 6.283185307179586,
+          "target": "2-veev_fab_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.14002295122992,
+          "pitch": 0.37548855636710776,
+          "title": "Veev Build",
+          "text": "Union City, CA, USA"
+        }
+      ]
     },
     {
-      "id": "1-veev_fab_001",
-      "name": "veev_fab_001",
+      "id": "1-veev_fab_1",
+      "name": "veev_fab_1",
       "levels": [
         {
           "tileSize": 256,
@@ -63,16 +83,29 @@ var APP_DATA = {
       ],
       "faceSize": 2586,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -2.1036004779070225,
+        "pitch": 0.08033139781840681,
+        "fov": 1.3108730938557924
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.4351753189966594,
+          "pitch": 0.4582899367432418,
+          "rotation": 6.283185307179586,
+          "target": "0-veev_fab_5"
+        },
+        {
+          "yaw": -2.227117193770452,
+          "pitch": 0.4999021839113915,
+          "rotation": 0,
+          "target": "2-veev_fab_2"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "2-veev_fab_002",
-      "name": "veev_fab_002",
+      "id": "2-veev_fab_2",
+      "name": "veev_fab_2",
       "levels": [
         {
           "tileSize": 256,
@@ -102,154 +135,20 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-veev_fab_003",
-      "name": "veev_fab_003",
-      "levels": [
+      "linkHotspots": [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          "yaw": 2.383486112025201,
+          "pitch": 0.5734303756963648,
+          "rotation": 0,
+          "target": "0-veev_fab_5"
         },
         {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
+          "yaw": -2.1597825727888846,
+          "pitch": 0.5995065394447412,
+          "rotation": 0,
+          "target": "1-veev_fab_1"
         }
       ],
-      "faceSize": 2586.5,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-veev_fab_004",
-      "name": "veev_fab_004",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 2586.5,
-      "initialViewParameters": {
-        "yaw": -1.3408348393530005,
-        "pitch": 0.1821429828101806,
-        "fov": 0.6834750465055764
-      },
-      "linkHotspots": [],
-      "infoHotspots": [
-        {
-          "yaw": -1.3329567795598898,
-          "pitch": 0.19373509491034824,
-          "title": "Veev Build",
-          "text": "Union City, CA, USA"
-        }
-      ]
-    },
-    {
-      "id": "5-veev_fab_005",
-      "name": "veev_fab_005",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 2587,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-veev_fab_006",
-      "name": "veev_fab_006",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 2587,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
@@ -258,6 +157,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
